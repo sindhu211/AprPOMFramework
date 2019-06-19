f767609867871a6e07d45c7a27f039d5be64dd7c
mgova <mgova@LAPTOP-BH638UES>
2019-06-19 15:40:54 +0530

    Updated Constands class and created DealsPage class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,10 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class DealsPage extends BasePage {
+	WebDriver driver;
+	public  DealsPage(WebDriver driver) {
+		this.driver=driver;
+}
+}
